3d954f545f26e3f142d5dbba06882de6f9d1adc8
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-03-22 20:55:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+//document.write("1"+1);
+// alert(2+4)
+// console.log("Hello World")
+
+// var num1 = 30;
+// var num2 = 40;
+// document.write(num1%num2);
+
+// var name1;
+// name1 = "Abdul Hadi"
+// document.write(name1)
+
+// document.write(2+2+(3*10));
+// var userName = "Maaz "
+// var banger = "bangan"
+// var message = "Anna Appa"
+// alert(banger + ' ' + userName + message);
+
+// var spec = prompt("Your species?","saad");
+// document.write(spec)
